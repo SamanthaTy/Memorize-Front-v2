d109d9e9c706ff06890ca3e219346c99d3e5b9b2
Etudiant O'clock <samantha.ty@oclock.school>
2024-02-08 16:01:27 +0100

    feat: Creation of Decks component

--- memorize-front/src/components/App/App.tsx
+++ memorize-front/src/components/App/App.tsx
@@ -3,6 +3,8 @@ import Footer from "../Footer";
 import Header from "../Header";
 import Modal from "react-modal";
 
+import Decks from "../Decks"
+
 function App() {
   Modal.setAppElement("#root");
 
