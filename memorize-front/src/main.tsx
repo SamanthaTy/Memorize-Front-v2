747d5dd24bb2b15ede42cc39a37df787dc5e8c30
Etudiant O'clock <samantha.ty@oclock.school>
2024-02-09 10:14:12 +0100

    feat: start creation of the router

--- memorize-front/src/main.tsx
+++ memorize-front/src/main.tsx
@@ -1,34 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App.tsx";
-import Decks from "./components/Decks/index.tsx";
-import Home from "./components/Home/index.tsx";
-import Cards from "./components/Cards/index.tsx";
 import "./index.scss";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./store/index.ts";
-import Error from "./components/Error/index.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    errorElement: < Error />,
-
+    errorElement: <Error />,
     children: [
-      {index: true, element: <Home />},
-      {path: "/decks/:id", element: <Cards />},
-      {path: "/decks", element: <Decks />},
-    ]
-
-  }
-])
+      { index: true, element: <Home /> },
+      { path: "/decks", element: <Decks /> },
+      { path: "/decks/:id", element: <Cards /> }, // (user_id)
+      { path: "/profile", element: <Profile /> },
+      { path: "/decks/:id/trainingsession", element: <TrainingSession /> },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
