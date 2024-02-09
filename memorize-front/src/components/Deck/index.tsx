beffa919d5164304c7a33cd6428ebc721725a7bc
Etudiant O'clock <samantha.ty@oclock.school>
2024-02-09 11:07:27 +0100

    styles: added style on decks buttons

--- memorize-front/src/components/Deck/index.tsx
+++ memorize-front/src/components/Deck/index.tsx
@@ -3,22 +3,24 @@ import "./styles.scss";
 
 const Deck = () => {
   return (
-    <div className="flip-card">
-    <div className="flip-card-inner">
-        <div className="flip-card-front">
-            <h3 className="title">Nom du deck</h3>
-            <p>15 cartes</p>
-            <ul>
-              <li>Easy : 5</li>
-              <li>Medium : 5</li>
-              <li>Hard : 5</li>
-            </ul>
-            <button className="session-btn">START</button>
-            <button className="edit-btn">Modifier</button>
-            <button className="delete-btn">Supprimer</button>
+    <div className="decks-container flex">
+      <div className="flip-card">
+        <div className="flip-card-inner">
+            <div className="flip-card-front">
+                <h3 className="title">Nom du deck</h3>
+                <p>15 cartes</p>
+                <ul>
+                  <li>Easy : 5</li>
+                  <li>Medium : 5</li>
+                  <li>Hard : 5</li>
+                </ul>
+                <button className="session-btn border-2 rounded-full border-white p-5 m-5">START</button>
+                <button className="edit-btn p-2 m-2">Modifier</button>
+                <button className="delete-btn p-2 m-2">Supprimer</button>
+            </div>
         </div>
+      </div>
     </div>
-</div>
   )
 }
 
