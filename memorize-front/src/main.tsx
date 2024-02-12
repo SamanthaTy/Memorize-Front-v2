6dfc59b21855ca0be66342b9da227749b6ed520b
Etudiant O'clock <samantha.ty@oclock.school>
2024-02-12 11:06:14 +0100

    feat: updated routes for Decks and Error

--- memorize-front/src/main.tsx
+++ memorize-front/src/main.tsx
@@ -1,20 +1,25 @@
-import React from 'react'
+import React, { Profiler } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App/App.tsx'
 import './index.scss'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Error from './components/Error/index.tsx';
+import Home from './components/Home/index.tsx';
+import Decks from './components/Decks/index.tsx';
+import Cards from './components/Cards/index.tsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     errorElement: <Error />,
+
     children: [
       {index: true, element: <Home />},
       {path: "/decks", element: <Decks />},
       {path: "/decks/:id", element: <Cards />}, // (user_id)
-      {path: "/profile", element: <Profile />},
-      {path: "/decks/:id/trainingsession", element: <TrainingSession />},
+      //{path: "/profile", element: <Profile />},
+      //{path: "/decks/:id/trainingsession", element: <TrainingSession />},
     ]
   }
 ])
