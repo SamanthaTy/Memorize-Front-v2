ea8e75cf9ed3d893a2cb090e808adfbe3ad60d11
Etudiant O'clock <samantha.ty@oclock.school>
2024-02-12 10:45:59 +0100

    refactor: added Deck component to Decks

--- memorize-front/src/components/Decks/index.tsx
+++ memorize-front/src/components/Decks/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Deck from "./Deck";
 
 const Decks = () => {
 
@@ -10,6 +11,9 @@ const Decks = () => {
         <div className="search-container">
           <input type="search" placeholder="Chercher les decks" className="border-2 border-gray-300 p-2 mt-2 rounded-md"/>
           <button className="bg-blue-500 text-white p-2 mt-2 rounded-md">Search</button>
+        </div>
+        <div>
+          <Deck />
         </div>
       </div>  
     </>
