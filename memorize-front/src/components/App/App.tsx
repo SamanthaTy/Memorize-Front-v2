e1bd0547e3c7c7238b668d58125f009a4974d92a
Etudiant O'clock <samantha.ty@oclock.school>
2024-02-12 10:20:26 +0100

    styles: some rework of the Decks css, to be finished

--- memorize-front/src/components/App/App.tsx
+++ memorize-front/src/components/App/App.tsx
@@ -1,7 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../Footer";
 import Header from "../Header";
-import Decks from "../Decks";
 
 import Modal from "react-modal";
 
@@ -14,11 +13,6 @@ function App() {
     <div >
       <Header />
       <main className="flex flex-col justify-items-center">
-        <Introduction />
-        <LoginForm />
-        <Cards />
-        <Card />
-        <Decks />
         <Outlet />
       </main>
       <Footer />
