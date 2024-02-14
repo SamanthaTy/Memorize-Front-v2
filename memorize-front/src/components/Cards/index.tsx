795c5bad86f09513aa350619f6817d4b8ae75b0e
FahimLcq <fahim.leclercq@oclock.school>
2024-02-14 22:18:20 +0100

    feat: modale CRUD card

--- memorize-front/src/components/Cards/index.tsx
+++ memorize-front/src/components/Cards/index.tsx
@@ -1,17 +1,48 @@
+import { useAppDispatch, useAppSelector } from "../../Hooks/redux";
+import { toggleModal } from "../../store/actions/modal";
 import Card from "./Card";
+import CreateCardModal from "./CreateCardModal";
+import DeleteCardModal from "./DeleteCardModal";
+import EditCardModal from "./EditCardModal";
 
 function Cards() {
+  const dispatch = useAppDispatch();
+  const modalIsOpen = useAppSelector((state) => state.modal.modalIsOpen);
+  const modalType = useAppSelector((state) => state.modal.modalType);
+
+  const handleToggleCreateClick = () => {
+    dispatch(toggleModal({ modalType: "create" }));
+  };
+
+  const handleToggleEditClick = () => {
+    dispatch(toggleModal({ modalType: "edit" }));
+  };
+
+  const handleToggleDeleteClick = () => {
+    dispatch(toggleModal({ modalType: "delete" }));
+  };
+
   return (
     <main className="container mx-auto p-4">
       <div className="flex space-x-4 mb-4">
-        <button className="bg-blue-500 text-white px-4 py-2 rounded">
+        <button
+          onClick={handleToggleEditClick}
+          className="bg-blue-500 text-white px-4 py-2 rounded"
+        >
           Edit
         </button>
+
         <h2 className="text-3xl font-bold">Nom du Deck</h2>
-        <button className="bg-red-500 text-white px-4 py-2 rounded">
+        <button
+          onClick={handleToggleDeleteClick}
+          className="bg-red-500 text-white px-4 py-2 rounded"
+        >
           Delete
         </button>
-        <button className="bg-blue-500 text-white px-4 py-2 rounded">
+        <button
+          onClick={handleToggleCreateClick}
+          className="bg-blue-500 text-white px-4 py-2 rounded"
+        >
           Nouvelle Carte
         </button>
 
@@ -21,6 +52,15 @@ function Cards() {
           className="border p-2 rounded"
         />
       </div>
+
+      {modalIsOpen && (
+        <>
+          {modalType === "edit" && <EditCardModal />}
+          {modalType === "create" && <CreateCardModal />}
+          {modalType === "delete" && <DeleteCardModal />}
+        </>
+      )}
+
       <Card />
     </main>
   );
