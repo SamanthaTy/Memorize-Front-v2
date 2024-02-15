8656bb198bbdb5b704871a453f3256cf8711f7cd
Etudiant O'clock <samantha.ty@oclock.school>
2024-02-15 10:23:38 +0100

    feat: added .env for API URL

--- memorize-front/src/store/actions/login.ts
+++ memorize-front/src/store/actions/login.ts
@@ -10,7 +10,7 @@ export const tokenCheck = createAction(TOKEN_CHECK)
 
 // the thunk allows us to create an action to get data from the API
 export const login = createAsyncThunk(LOGIN, async(formData) => {
-  const response = await axios.post("http://127.0.0.1:3000/api/auth/", formData, {
+  const response = await axios.post(`${import.meta.env.VITE_API_URL}/auth/`, formData, {
     headers: { Authorization: `Bearer ${localStorage.getItem("accessToken")}` },
   })
   console.log("Response from login action:", response.data);
