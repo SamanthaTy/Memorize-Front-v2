be007d902ec8c5a39584c38b92dbb9e8d3edd15d
Etudiant O'clock <samantha.ty@oclock.school>
2024-02-15 14:44:32 +0100

    feat: created Edit Deck Modal with state and dynamised the modal title for Edit and Delete Deck

--- memorize-front/src/components/Decks/Deck/DeleteDeckModal/index.tsx
+++ memorize-front/src/components/Decks/Deck/DeleteDeckModal/index.tsx
@@ -2,7 +2,7 @@ import ModalContainer from "../../../ModalContainer";
 
 const DeleteDeckModal = ({isOpen, onClose} : {isOpen: boolean, onClose: () => void}) => {
   return (
-    <ModalContainer isOpen={isOpen} onClose={onClose}>
+    <ModalContainer isOpen={isOpen} onClose={onClose} modalTitle="Supprimer le deck">
       <h2>Êtes-vous sûr de vouloir supprimer ce deck et ses cartes ?</h2>
     </ModalContainer>
   )
