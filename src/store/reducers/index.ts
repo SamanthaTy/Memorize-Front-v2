63110a7e11ed34476c961405aa4a0c90538f900a
Etudiant O'clock <samantha.ty@oclock.school>
2024-02-20 17:06:37 +0100

    feat: create user feature, ok

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,11 +1,13 @@
 import modalReducer from "./modal";
 import loginReducer from "./login";
 import decksReducer from "./decks";
+import createUserReducer from "./user";
 
 const reducer = {
   modal: modalReducer,
   login: loginReducer,
   decks: decksReducer,
+  user: createUserReducer,
 };
 
 export default reducer;
