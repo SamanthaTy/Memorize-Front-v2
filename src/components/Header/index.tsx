a8b97509fe9c14a3d8e81180c84799a3ad8b24ec
FahimLcq <fahim.leclercq@oclock.school>
2024-02-21 12:54:19 +0100

    fix: css header div button reponsive

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,57 +3,55 @@ import { useAppDispatch, useAppSelector } from "../../hooks/redux";
 import { logout } from "../../store/actions/login";
 
 const Header = () => {
-
-// Use the state stored in our reducer so the button "Déconnection" and the welcome text only appear when the user is logged in. 
+  // Use the state stored in our reducer so the button "Déconnection" and the welcome text only appear when the user is logged in.
   const dispatch = useAppDispatch();
   const isLogged = useAppSelector((state) => state.login.isLogged);
-  console.log(isLogged)
- 
+  console.log(isLogged);
+
   const username = useAppSelector((state) => state.login.username);
 
   const handleDisconnectClick = () => {
     dispatch(logout());
-  }
+  };
 
   return (
-    <>
-      <header className="header-container w-screen flex justify-between h-10 border-b-4 box-content py-10">
-        <div className="flex items-center">
-          <a href="/">
-            <img src={logo} alt="Logo Mem'O'rize" className="flex" />
-          </a>
-          <a href="/">
-          <h1 className="title flex">MEM'O'RIZE</h1>
-          </a>
-        </div>
-        <div className="btn-container flex content-end">
-          <a 
-            className="btn-header border-2 px-5 pt-1.5 mx-5 border-black rounded-md"
-            href="/profile">
-            Mon compte
-          </a>
+    <header className="header-container w-screen flex justify-between h-16 border-b-4 box-content p-4">
+      <div className="flex items-center">
+        <a href="/">
+          <img src={logo} alt="Logo Mem'O'rize" className="flex" />
+        </a>
+        <a href="/">
+          <h1 className="title flex text-xl font-semibold ml-2">MEM'O'RIZE</h1>
+        </a>
+      </div>
+      <div className="btn-container flex items-center space-x-4">
+        <a
+          className="btn-header border-2 px-4 py-2 border-black rounded-md"
+          href="/profile"
+        >
+          Mon compte
+        </a>
 
-          <a 
-            className="btn-header border-2 px-5 pt-1.5 mx-5 border-black rounded-md"
-            href="/decks">
-            Mes decks
-          </a>
+        <a
+          className="btn-header border-2 px-4 py-2 border-black rounded-md"
+          href="/decks"
+        >
+          Mes decks
+        </a>
 
-          {isLogged && 
-            <button 
-              className="btn-header border-2 px-5 mx-5 border-black rounded-md"
+        {isLogged && (
+          <>
+            <button
+              className="btn-header border-2 px-4 py-2 border-black rounded-md"
               onClick={handleDisconnectClick}
             >
               Déconnexion
             </button>
-          }
-          {isLogged && 
-            <p>Bienvenue {username}</p>         
-          }
-         
-        </div>
-      </header>
-    </>
+            <p className="text-white">Bienvenue {username}</p>
+          </>
+        )}
+      </div>
+    </header>
   );
 };
 
