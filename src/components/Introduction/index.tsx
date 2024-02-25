7d8ae5cab663c815a252934a68bab35836b442a6
Etudiant O'clock <samantha.ty@oclock.school>
2024-02-25 14:10:59 +0100

    styles: intro text added

--- src/components/Introduction/index.tsx
+++ src/components/Introduction/index.tsx
@@ -3,12 +3,18 @@ function Introduction() {
     <>
     <div className="flex items-center justify-center">
       <div className="w-4/5 md:w-auto border-2 p-4 m-4">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora
-          deleniti odio amet nihil corporis assumenda ea! Nobis, quae quibusdam
-          cum voluptatibus quis ratione obcaecati nulla consectetur dolorem unde
-          quisquam doloribus.
-        </p>
+        <p>T'en as marre d'écrire des flashcards pour réviser et de te retrouver sous une montagne de papier... ? </p>
+        <p>On a la solution pour toi ! </p>
+        <br/>
+        <p>Mem'O'rize te permet de créer et stocker des decks de flashcards pour tous les thèmes de ton choix. Tu pourras ensuite lancer une session d'entraînement de 15 cartes. 
+        Durant ta session, une fois que tu as la réponse, retoune la carte et sélectionne si la question était "Facile", "Moyenne", "Difficile". Si la carte est notée comme "Difficile", la carte retourne dans le deck de la session pour revenir plus tard, jusqu'à ce qu'elle soit notée "Facile" ou "Moyenne". 
+        Et hop, tu as fini ta session ! *clap clap* </p>
+        <br/>
+        <p>À la fin, tu as un petit recap de ta session et tes cartes ont enregistrées tes résultats pour que la prochaine session te  quiz sur les cartes qui t'ont posées le plus de problème. </p>
+        <br/>
+        <p>Tu peux t'entrainer à l'infini et créer autant de decks que tu le souhaites. Plus de papier gaspillé, plus de prise de tête ! </p>
+        <br/>
+        <p>Mem'O'rize, ton meilleur outils de révision ! </p>
       </div>
     </div>
     </>
