880bf93004958a0cc8dd1dfe026f8f87e8cb7f9b
Aurélien F <85134884+aurelienflieger@users.noreply.github.com>
2024-02-27 10:03:22 +0100

    Revert "Feature/deck dynamic"

--- src/components/ModalContainer/index.tsx
+++ src/components/ModalContainer/index.tsx
@@ -1,7 +1,6 @@
 import { FormEventHandler } from "react";
 import Modal from "react-modal";
 import { Styles } from "react-modal";
-import editLogo from "/home/student/Bureau/html/Mem'O'rize/projet-09-memorize-front/src/assets/pencil.png";
 
 export interface ModalProps {
   isOpen: boolean;
@@ -77,12 +76,9 @@ function ModalContainer({
               Annuler
             </button>
 
-            <img 
-              src={editLogo}
-              className="h-10 px-4 py-2 bg-1F3D75 text-F5E9E0 rounded transition-colors duration-300 ease-in-out hover:bg-F5E9E0 hover:text-1F3D75"
-            />
-              
-            
+            <button className="px-4 py-2 bg-1F3D75 text-F5E9E0 rounded transition-colors duration-300 ease-in-out hover:bg-F5E9E0 hover:text-1F3D75">
+              Confirmer
+            </button>
           </div>
         </form>
       </Modal>
