708fe7717afdc7162e6f398608cfe4bcbe2b4899
FahimLcq <fahim.leclercq@oclock.school>
2024-02-27 21:33:30 +0100

    styles: loginform css fix

--- src/components/Introduction/index.tsx
+++ src/components/Introduction/index.tsx
@@ -1,16 +1,16 @@
 function Introduction() {
   return (
     <>
-    <div className="flex items-center justify-center">
-      <div className="w-4/5 md:w-auto border-2 p-4 m-4">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora
-          deleniti odio amet nihil corporis assumenda ea! Nobis, quae quibusdam
-          cum voluptatibus quis ratione obcaecati nulla consectetur dolorem unde
-          quisquam doloribus.
-        </p>
+      <div className="flex items-center justify-center">
+        <div className="w-4/5 md:w-auto border-2 p-4 m-4">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora
+            deleniti odio amet nihil corporis assumenda ea! Nobis, quae
+            quibusdam cum voluptatibus quis ratione obcaecati nulla consectetur
+            dolorem unde quisquam doloribus.
+          </p>
+        </div>
       </div>
-    </div>
     </>
   );
 }
