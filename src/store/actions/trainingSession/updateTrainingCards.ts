7972da1ba2cba605b53ea12495b923ae48b2998d
aurelienflieger <aurelien.flieger@protonmail.com>
2024-02-28 01:00:20 +0100

    fix: auth headers in updateDifficulties

--- src/store/actions/trainingSession/updateTrainingCards.ts
+++ src/store/actions/trainingSession/updateTrainingCards.ts
@@ -1,31 +1,27 @@
-import { createAction, createAsyncThunk } from "@reduxjs/toolkit";
+import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
-import { Card } from "../../reducers/trainingSession";
-
+import { getAuthHeaders, updateTokens } from "../utils.actions";
 
 export const UPDATE_DIFFICULTIES = "UPDATE_DIFFICULTIES";
 
+export const updateTrainingCards = createAsyncThunk<any, string>(
+  UPDATE_DIFFICULTIES,
+  async (deckId, updatedDifficulties) => {
+    const userId = localStorage.getItem("id");
 
-export const updateTrainingCards = createAsyncThunk<any, string>(UPDATE_DIFFICULTIES, async (deckId, updatedDifficulties) => {
-  const userId = localStorage.getItem("id");
- 
-  
-  if (userId) {
-    const response = await axios.patch(
-      `${import.meta.env.VITE_API_URL}/account/${userId}/decks/${deckId}/cards/update-difficulties`,
-      updatedDifficulties,
-      {
-        headers: {
-          authorization: localStorage.getItem("accessToken"),
-        },
-      }
-    );
-    console.log(response.data);
-    return response.data;
-  } else {
-    console.log("There was an error fecthing the data.")
+    if (userId) {
+      const response = await axios.patch(
+        `${
+          import.meta.env.VITE_API_URL
+        }/account/${userId}/decks/${deckId}/cards/update-difficulties`,
+        updatedDifficulties,
+        getAuthHeaders()
+      );
+      updateTokens(response);
+      console.log(response.data);
+      return response.data;
+    } else {
+      console.log("There was an error fecthing the data.");
+    }
   }
-});
-
-
-
+);
